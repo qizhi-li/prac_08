ff95afe9257af468ed13a51a5500dddb72c5958d
qizhli <qizhi.li@my.jcu.edu.au>
2022-05-15 11:33:20 +0800

    Y

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -19,8 +19,8 @@ class Taxi(Car):
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
         return "{}, {}km on current fare, ${:.2f}/km".format(super().__str__(),
-                                                             self.current_fare_distance,
-                                                             self.price_per_km)
+        self.current_fare_distance,
+        self.price_per_km)
 
     def get_fare(self):
         """Return the price for the taxi trip."""
